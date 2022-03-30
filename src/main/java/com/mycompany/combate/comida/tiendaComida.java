1b8784377cb8dc78bf2ded42a32778cd1278ec5e
StarkBryan <bryanicloud100@gmail.com>
2022-03-30 06:19:50 -0600

    las mnuecas clases comida con lo de adentro

---
+++ src/main/java/com/mycompany/combate/comida/tiendaComida.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.combate.comida;
+
+import com.mycompany.comida.comida;
+
+/**
+ *
+ * @author ASUS
+ * 
+ */
+public class tiendaComida {
+    
+    comida comida= new comida();
+    
+      protected String nombre;
+      
+      public comida ElegirComida(int ronda ){
+      
+      
+      
+      
+      return comida;
+      }
+      
+    
+}
