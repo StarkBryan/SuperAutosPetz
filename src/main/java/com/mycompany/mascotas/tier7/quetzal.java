26cfea0077fd7fb8a7b702f05b019d2ecd39a422
StarkBryan <bryanicloud100@gmail.com>
2022-03-31 12:03:53 -0600

    agregamos todas las clases animales

---
+++ src/main/java/com/mycompany/mascotas/tier7/quetzal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mascotas.tier7;
+
+import com.mycompany.mascotas.mascotas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class quetzal  extends mascotas{
+     public quetzal(){
+         nombre = "quetzal";
+      danio=10;
+       vida=10;
+       efecto = false; 
+       tipo ="volador  solitario " ;
+       
+    }
+    
+    
+}
